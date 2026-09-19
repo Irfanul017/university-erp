03a48419938fb0ef89c387845f7a94b3c00c49a3
Sachin Jha <sjha04180@gmail.com>
2026-09-19 21:23:20 +0530

    feat(department): fix delete route

--- src/main/java/com/university/erp/department/repository/DepartmentRepository.java
+++ src/main/java/com/university/erp/department/repository/DepartmentRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.university.erp.department.entity.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
     boolean existsByDepartmentNameIgnoreCase(String departmentName);
+
 }
